8c2ecee27285a35ed9de9d4d988cc474d6dd0f02
Starkov Sergey <starkovsr@gmail.com>
2025-02-17 10:57:33 +0300

    refactor: update imports and add path aliases for cleaner module resolution

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { App } from './app/App.tsx'
 import { Provider } from 'react-redux'
-import { store } from './app/store.ts'
+import { store } from '@/app/store.ts'
+import { App } from '@/app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
