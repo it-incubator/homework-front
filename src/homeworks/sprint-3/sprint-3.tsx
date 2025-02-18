cfa1b0cd4dcfdace0bf82a1360ee65996b5939d4
Starkov Sergey <starkovsr@gmail.com>
2025-02-18 14:26:23 +0300

    feat: add hw-12 (dates and time)

--- src/homeworks/sprint-3/sprint-3.tsx
+++ src/homeworks/sprint-3/sprint-3.tsx
@@ -1,6 +1,7 @@
 import { Homework9 } from './hw-9/Homework9.tsx'
 import { Homework10 } from './hw-10/Homework10.tsx'
 import { Homework11 } from '@/homeworks/sprint-3/hw-11/Homework11.tsx'
+import { Homework12 } from '@/homeworks/sprint-3/hw-12/Homework12.tsx'
 
 export const Sprint3 = () => {
   return (
@@ -9,6 +10,7 @@ export const Sprint3 = () => {
       <Homework9 />
       <Homework10 />
       <Homework11 />
+      <Homework12 />
     </div>
   )
 }
